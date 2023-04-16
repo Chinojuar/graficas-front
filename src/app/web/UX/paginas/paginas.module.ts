889663612640ec362c7100d4aab6a6528e4e7ae4
luis.juarez <79300349+Chinojuar@users.noreply.github.com>
2023-04-16 01:44:20 -0600

    creacion de modulos de compra,ventas,gastos

--- src/app/web/UX/paginas/paginas.module.ts
+++ src/app/web/UX/paginas/paginas.module.ts
@@ -4,11 +4,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/web/informacion/utils/shared.module';
 import { RegistroComponent } from './registro/registro.component';
 import { LoginComponent } from './login/login.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { GastosComponent } from './gastos/gastos.component';
+import { ComprasComponent } from './compras/compras.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegistroComponent,
+    VentasComponent,
+    GastosComponent,
+    ComprasComponent,
   ],
   imports: [
     CommonModule,
